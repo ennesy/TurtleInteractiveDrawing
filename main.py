d6244802327485a63a078d94c34feb5b4589039e
ennesy <ennesy7@gmail.com>
2024-08-15 18:53:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("white")
+drawing_board.title("Python Turtle")
+
+turtle_instance = turtle.Turtle()
+
+def turtle_forward():
+    turtle_instance.forward(100)
+
+def rotate_angle_right():
+    turtle_instance.right(20)
+
+def rotate_angle_left():
+    turtle_instance.left(20)
+
+def clear_screen():
+    turtle_instance.clear()
+
+def turtle_return_home():
+    turtle_instance.home()
+
+def turtle_pen_up():
+    turtle_instance.penup()
+
+def turtle_pen_down():
+    turtle_instance.pendown()
+
+drawing_board.listen()
+drawing_board.onkey(fun=turtle_forward, key="space")
+drawing_board.onkey(fun=rotate_angle_right, key="Down")
+drawing_board.onkey(fun=rotate_angle_left, key="Up")
+drawing_board.onkey(fun=clear_screen, key="c")
+drawing_board.onkey(fun=turtle_return_home, key="h")
+drawing_board.onkey(fun=turtle_pen_up, key="w")
+drawing_board.onkey(fun=turtle_pen_down, key="q")
+
+turtle.mainloop()
